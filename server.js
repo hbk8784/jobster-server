5bb42881ce2d71a4121ad617dea0bc2ca7d3b3bb
hatif <hb8784@gmail.com>
2024-03-01 06:34:51 +0530

    cors settings

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.use(
   cors({
     origin: "http://localhost:3000", // Allow requests from this origin
     methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
   })
 );
 // app.use(helmet());
