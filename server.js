9d7ec73665ce3ff09e8cdc2c9650653b43311bf1
hatif <hb8784@gmail.com>
2024-03-01 14:13:44 +0530

    changes to cors

--- server.js
+++ server.js
@@ -21,9 +21,7 @@ const cors = require("cors");
 // );
 app.use(
   cors({
-    origin: "http://127.0.0.1:3000", // Allow requests from this origin
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    allowedHeaders: ["Authorization", "Content-Type"],
+    origin: "http://localhost:3000",
   })
 );
 // app.use(helmet());
