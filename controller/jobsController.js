760a4c96b58a3412bcb556536ac4d766b4d7cf01
hatif <hb8784@gmail.com>
2024-03-02 00:47:15 +0530

    sorting and pagination not working, so had to remove them

--- controller/jobsController.js
+++ controller/jobsController.js
@@ -34,16 +34,17 @@ async function getAllJobs(req, res) {
     //   jobs = await v.sort("-position");
     // }
 
-    const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 10;
-    const skip = (page - 1) * limit;
+    // const page = Number(req.query.page) || 1;
+    // const limit = Number(req.query.limit) || 10;
+    // const skip = (page - 1) * limit;
 
-    jobs = await jobs.skip(skip).limit(limit);
+    // jobs = await jobs.skip(skip).limit(limit);
 
-    const totalJobs = await jobSchema.countDocuments(queryObject);
-    const numOfPages = Math.ceil(totalJobs / limit);
+    // const totalJobs = await jobSchema.countDocuments(queryObject);
+    // const numOfPages = Math.ceil(totalJobs / limit);
 
-    res.status(200).json({ jobs, totalJobs, numOfPages });
+    // res.status(200).json({ jobs, totalJobs, numOfPages });
+    res.status(200).json({ jobs });
   } catch (error) {
     res.status(400).json({ message: error.msg });
   }
