f53fa7f9f305db0917369e836de23bfba7a557f0
hatif <hb8784@gmail.com>
2024-03-01 17:18:38 +0530

    jobs controller

--- controller/jobsController.js
+++ controller/jobsController.js
@@ -4,52 +4,54 @@ const mongoose = require("mongoose");
 
 async function getAllJobs(req, res) {
   try {
-    const { search, status, jobType, sort } = req.query;
-    console.log(req.query);
+    // const { search, status, jobType, sort } = req.query;
+    // console.log(req.query);
 
-    const queryObject = {
-      createdBy: req.user.id,
-    };
+    // const queryObject = {
+    //   createdBy: req.user.id,
+    // };
 
-    if (search) {
-      queryObject.position = { $regex: search, $options: "i" };
-    }
+    // if (search) {
+    //   queryObject.position = { $regex: search, $options: "i" };
+    // }
 
-    if (status && status !== "all") {
-      queryObject.status = status;
-    }
-    if (jobType && jobType !== "all") {
-      queryObject.jobType = jobType;
-    }
-    let result = await jobSchema.find(queryObject);
+    // if (status && status !== "all") {
+    //   queryObject.status = status;
+    // }
+    // if (jobType && jobType !== "all") {
+    //   queryObject.jobType = jobType;
+    // }
+    // let result = await jobSchema.find(queryObject);
 
-    if (sort === "latest") {
-      result = await result.sort("-createdAt");
-    }
-    if (sort === "oldest") {
-      result = await result.sort("createdAt");
-    }
-    if (sort === "a-z") {
-      result = await result.sort("position");
-    }
-    if (sort === "z-a") {
-      result = await result.sort("-position");
-    }
+    // if (sort === "latest") {
+    //   result = await result.sort("-createdAt");
+    // }
+    // if (sort === "oldest") {
+    //   result = await result.sort("createdAt");
+    // }
+    // if (sort === "a-z") {
+    //   result = await result.sort("position");
+    // }
+    // if (sort === "z-a") {
+    //   result = await result.sort("-position");
+    // }
 
-    const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 10;
-    const skip = (page - 1) * limit;
+    // const page = Number(req.query.page) || 1;
+    // const limit = Number(req.query.limit) || 10;
+    // const skip = (page - 1) * limit;
 
-    result = await result.skip(skip).limit(limit);
+    // result = await result.skip(skip).limit(limit);
 
-    const jobs = await result;
+    // const jobs = await result;
 
-    const totalJobs = await jobSchema.countDocuments(queryObject);
-    const numOfPages = Math.ceil(totalJobs / limit);
+    // const totalJobs = await jobSchema.countDocuments(queryObject);
+    // const numOfPages = Math.ceil(totalJobs / limit);
 
-    res.status(200).json({ jobs, totalJobs, numOfPages });
+    const jobs = jobSchema.findById({ createdBy: req.user.id });
+
+    res.status(200).json({ jobs });
   } catch (error) {
-    res.status(400).json({ message: error });
+    res.status(400).json({ message: error.msg });
   }
 }
 
